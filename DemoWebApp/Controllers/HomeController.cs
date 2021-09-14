b48126e0f4bb46ef67d6c1c20eb0280d4824ffcd
Gergő M <matyus.gergo@hmei.hu>
2021-09-14 15:41:54 +0200

    Add project files.

---
+++ DemoWebApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using DemoWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IOptions<EnvironmentModel> _appSettings;
+
+		public HomeController(ILogger<HomeController> logger, IOptions<EnvironmentModel> appSettings)
+		{
+			_logger = logger;
+			_appSettings = appSettings;
+		}
+
+		public IActionResult Index()
+		{
+			ViewBag.Environment = _appSettings.Value;
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ DemoWebApp/Models/EnvironmentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebApp.Models
+{
+	public class EnvironmentModel
+	{
+		public string Environment { get; set; }
+		public string Deployment { get; set; }
+	}
+}
